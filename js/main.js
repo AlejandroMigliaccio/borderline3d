5894cf3b8af6cb9de4d6baa2ffbee1ddd3bf64e7
AlejandroMigliaccio <102888662+AlejandroMigliaccio@users.noreply.github.com>
2022-06-05 18:27:03 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let product ={
+    id: Number,
+    name: String,
+    price: Number
+}
+
+const itemsAmount = () => {
+    debugger
+    let total = 0;
+    let item = parseInt(prompt('Ingrese la cantidad a llevar: '));
+    while(item <= 0){
+         item = parseInt(prompt('Por favor ingrese al menos 1 item'));
+    }
+    return additem(item, total)
+}
+
+const additem = (items, total) => {
+    debugger
+    total += items;
+    return total;
+}
+
+let total= itemsAmount()
